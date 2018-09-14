c08acab23fa230d148ab1aed78f0ed91f79498a8
Michael Foland <michaelfoland@gmail.com>
2018-09-14 17:49:38 -0400

    Add basic styling of word buttons and update of selectedWords state on click

--- src/WordGroup.jsx
+++ src/WordGroup.jsx
@@ -1,12 +1,18 @@
 export class WordGroup extends React.Component {
+  handleClick = (e) => {
+    this.props.selectWord(e.target.dataset.word);
+  }
+  
+  isSelected = (word) => {
+    return (this.props.selectedWords.includes(word) ? 'selected' : '');
+  }
+  
   render() {
-    console.log('WordGroup.render()');
-    console.log('this.props =');
-    console.log(this.props);
-    
+    console.log('rendering word-group; selectedWords =',this.props.selectedWords);
     return (
-      <div>
-        {this.props.words.map((word, i) => <p key={i}>{word}</p>)}
+      <div className="word-group">
+        {this.props.words.map((word, i) => 
+          <span className={'word ' + this.isSelected(word)} data-word={word} onClick={this.handleClick} key={i}>{word}</span>)}
       </div>	
     );
   }
--- src/WordSelector.jsx
+++ src/WordSelector.jsx
@@ -4,9 +4,19 @@ export class WordSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visibleWords: []
+      visibleWords: [],
+      selectedWords: []
     }
   }
+
+  selectWord = (word) => {
+    if (this.state.selectedWords.includes(word)) return; // bail if word is already included
+    
+    this.setState(prevState => ({
+      selectedWords: prevState.selectedWords.concat(word)
+    }));
+  }
+
   
   handleClick = (e) => {
     const unit = this.props.units.find(unit => {
@@ -28,7 +38,7 @@ export class WordSelector extends React.Component {
     return (
       <div className="word-selector">
         {this.props.units.map((unit, i) => <button key={i} data-unit={unit.unit} onClick={this.handleClick}>Unit {unit.unit}</button>)}
-        <WordGroup words={this.state.visibleWords}/>
+        <WordGroup words={this.state.visibleWords} selectWord={this.selectWord} selectedWords={this.state.selectedWords} />
       </div>
     );
   }
